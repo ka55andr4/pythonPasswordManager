a419aefd4f007ce7a0ca7e3a4c6ec9346bf556c6
ka55andr4 <acostakass@gmail.com>
2023-09-13 20:13:14 -0500

    Add functions and file in append mode

--- password.py
+++ password.py
@@ -1,16 +1,29 @@
 #ask user for a master password
-password = input ("Enter master password")
+master_pwd = input("What is the master password?")
+
+#function for view mode 
+def view():
+    pass
+#function for add mode 
+def add():
+    #get username and and password to add
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    #file for password storing in append mode
+    with open('passwords.txt', 'a') as f:
+        f.write(name + " " + pwd)
 
 while True:
     #ask user if they want to list passwords or add a password
     mode = input("Would you like to add a new password or view passwords (view, add), press q to quit?").lower()
     if mode == "q":
         break
-    
+
     if mode == "view":
-        pass
+        view()
     elif mode == "add":
-        pass
+        add()
     else:
         print("Invalid input")
         continue
