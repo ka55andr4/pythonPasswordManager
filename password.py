c4a985439a08f99dbb426ea1b8aa8deaa3cfa5c3
ka55andr4 <acostakass@gmail.com>
2023-09-13 19:49:38 -0500

    Gather initial user input

--- password.py
+++ password.py
@@ -3,7 +3,10 @@ password = input ("Enter master password")
 
 while True:
     #ask user if they want to list passwords or add a password
-    mode = input("Would you like to add a new password or view passwords (view, add)?")
+    mode = input("Would you like to add a new password or view passwords (view, add), press q to quit?").lower()
+    if mode == "q":
+        break
+    
     if mode == "view":
         pass
     elif mode == "add":
